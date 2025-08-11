b5ad80e1b73e73e4a0da9ba66337b9bed6f5c26d
Charlie <charlie.mizutsuki@posteo.net>
2025-08-11 21:50:21 +0930

    Add finqwerty to build with latest signed APK from Github Actions

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 
 rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
-git clone https://github.com/chardidathing/treble_manifest.git .repo/local_manifests -b 15-los-qpr2
+git clone https://github.com/MP01Experiments/treble_manifest.git .repo/local_manifests -b 15-los-qpr2
 
 # this is destructive, but :shrug:
 repo sync --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune --force-checkout --force-remove-dirty -j8
 
-git clone https://github.com/chardidathing/MP01-LineageGSI.git MP01Support
+git clone https://github.com/MP01Experiments/MP01-LineageGSI.git MP01Support
 
 # Apply TrebleDroid patches
 cp -r ~/los22/MP01Support/patches ~/los22/patches
@@ -34,12 +34,31 @@ cd ~/los22
 cp ~/los22/MP01Support/treble_arm64* ~/los22/device/phh/treble/
 cp -r ~/los22/MP01Support/vendor ~/los22/
 cp -r ~/los22/MP01Support/vendor/inkOS ~/los22/vendor/
+cp -r ~/los22/MP01Support/vendor/finqwerty ~/los22/vendor/ # create finqwerty folder with Android.bp in it
 rm -rf ~/los22/MP01Support # Cleanup before build to prevent build errors
+
+# Download latest finqwerty apk
+finqwerty_apk="finqwerty-release.apk" # this will always stay the same
+
+# Fetch latest release JSON and extract download URL for the asset
+download_url=$(curl -s https://api.github.com/repos/MP01Experiments/finqwerty/releases/latest \
+  | jq -r --arg NAME "$finqwerty_apk" '.assets[] | select(.name == $NAME) | .browser_download_url')
+
+if [[ -z "$finqwerty_download_url" ]]; then
+  echo "Asset $finqwerty_apk not found in latest release." >&2
+  exit 1
+fi
+
+curl -L -o ~/los22/vendor/finqwerty/finqwerty-release.apk "$download_url" # Download latest release and dump in vendor/finqwerty
 
 # Do tha thing
 source ~/los22/build/envsetup.sh
-lunch treble_arm64_bvN-bp1a-userdebug
-make systemimage -j$(nproc --all)
+if ! lunch treble_arm64_bvN-bp1a-userdebug; then
+  exit 1
+fi
+if ! make systemimage -j$(nproc --all); then
+  exit 1
+fi
 
 build_date=$(date +%s)
 echo "Packing system image... build_date is ${build_date}"
