a75bedfe7a08fca71a16229a0e6ecafbd94686ea
Charlie <charlie.mizutsuki@posteo.net>
2025-08-12 17:39:11 +0930

    Restore ability to edit accessibility service with Android Studio, pray it still builds

---
+++ vendor/MP01_services/MP01_accessibility_service/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.application") version "8.2.0"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+}
+
+android {
+    namespace = "com.lmqr.hMP01_comp_service"
+    compileSdk = 34
+
+    sourceSets {
+        getByName("main") {
+            manifest.srcFile("./AndroidManifest.xml")
+            res.srcDirs("res")
+            java.srcDirs("src")
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.lmqr.hMP01_comp_service"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+
+        release {
+            isMinifyEnabled = true
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
